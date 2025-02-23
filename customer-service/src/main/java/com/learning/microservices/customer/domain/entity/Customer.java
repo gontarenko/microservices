7675a2e86fb3ec40d818b9358684c5d5c9fafed2
La1o <beeaveer@gmail.com>
2025-02-23 18:30:35 +0300

    modified migration

--- customer-service/src/main/java/com/learning/microservices/customer/domain/entity/Customer.java
+++ customer-service/src/main/java/com/learning/microservices/customer/domain/entity/Customer.java
@@ -10,7 +10,7 @@ import lombok.Data;
 public class Customer {
 
     @Id
-    @SequenceGenerator(name = "customer_id_seq", sequenceName = "customer_id_seq", allocationSize = 1)
+    @SequenceGenerator(name = "customer_id_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_seq")
     private Integer id;
     private String firstName;
--- customer-service/src/main/resources/db/migration/V1__Create_Customer_table.sql
+++ customer-service/src/main/resources/db/migration/V1__Create_Customer_table.sql
@@ -1,16 +1,6 @@
-create sequence customer_id_seq
-    start with 1
-    increment by 1
-    no minvalue
-    maxvalue 2147483647
-    cache 1;
-
-create table customer (
-    id integer not null default nextval('customer_id_seq'),
-    first_name varchar(255),
-    last_name varchar(255),
-    email varchar(255),
-    primary key (id)
+create table if not exists customer (
+    id serial primary key not null,
+    first_name varchar(255) not null,
+    last_name varchar(255) not null,
+    email varchar(255) not null unique
 );
-
-alter sequence customer_id_seq owned by customer.id;
